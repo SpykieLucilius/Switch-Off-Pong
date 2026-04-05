b9ae278bd6245e8e9d087568f122d071fc39044a
SpykieLucilius <spykie.lucilius@pm.me>
2026-04-05 17:48:39 +0300

    adding win screen + replay

--- main.gd
+++ main.gd
@@ -5,7 +5,21 @@ var score_p2 = 0
 
 @onready var label_p1 = $ScoreP1
 @onready var label_p2 = $ScoreP2
+@onready var victory_label = $VictoryLabel
+@onready var replay_button = $ReplayButton
 
+func check_win():
+	if score_p1 >= 5:
+		announce_winner("Player 1 Win !")
+	elif score_p2 >= 5:
+		announce_winner("Player 2 Win !")
+
+func announce_winner(message):
+	victory_label.text = message
+	$Ball.direction = Vector2.ZERO
+	$Ball.visible = false
+	replay_button.show()
+	
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -20,6 +34,7 @@ func _on_goal_left_body_entered(body: Node2D) -> void:
 		score_p2 += 1
 		label_p2.text = str(score_p2)
 		body.reset()
+		check_win()
 
 
 func _on_goal_right_body_entered(body: Node2D) -> void:
@@ -27,3 +42,17 @@ func _on_goal_right_body_entered(body: Node2D) -> void:
 		score_p1 += 1
 		label_p1.text = str(score_p1)
 		body.reset()
+		check_win()
+
+
+func _on_replay_button_pressed() -> void:
+	replay_button.hide()
+	victory_label.text = ""
+	
+	score_p1 = 0
+	score_p2 = 0
+	label_p1.text = "0"
+	label_p2.text = "0"
+	
+	$Ball.visible = true
+	$Ball.reset()
